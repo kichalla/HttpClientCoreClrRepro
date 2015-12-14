b56c951aedadd2b2493f4bb4a5f625714edad11f
Kiran Challa <kichalla@microsoft.com>
2015-12-14 09:48:21 -0800

    Repro

---
+++ src/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("http://localhost:5000/");
+            var response = httpClient.GetAsync("home/action1").Result;
+
+            Console.WriteLine(response);
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
---
+++ src/WebApp/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Text;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Primitives;
+
+namespace WebApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Action1()
+        {
+            Response.Cookies.Append("foo", "bar");
+
+            return RedirectToAction("Action2");
+        }
+
+        public IActionResult Action2()
+        {
+            var sb = new StringBuilder();
+            foreach (var cookie in Request.Cookies)
+            {
+                sb.Append($"{cookie.Key}:{cookie.Value}, ");
+            }
+
+            return Content(sb.ToString());
+        }
+    }
+}
